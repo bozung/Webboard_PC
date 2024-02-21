ec6417d0d8f904334dbf325854744ea5b43e64f5
Phatphonnicha <151551494+bozung@users.noreply.github.com>
2024-02-21 15:43:39 +0700

    WORK 7
    verify.php
    index.php
    nav.php
    login.php

--- index.php
+++ index.php
@@ -6,68 +6,69 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="bootstrap-5.3.2-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <title>WEB WEB WAM WAM</title>
+    <script src="bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js"></script>
+    <title style="text-align: center; font-size: 54px;">WebBoard KakKak</title>
 </head>
 <body>
     <div class="container-lg">
-    <h1 style="text-align: center;" class="mt-3">WEB WEB WAM WAM</h1>
-    <hr>
-    <nav class="navbar navbar-expand-lg " style="background-color: #d3d3d3;">
+    <h1 style="text-align: center;font-size: 54px;"class="mt-3">WebBoard ECT</h1>
+<nav class="navbar navbar-expand-lg" style="background-color:#d3d3d3;">
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.php"><i class="bi bi-house-door-fill"></i> HOME</a>
-
+    <a class="navbar-brand" href="index.php"><i class="bi bi-house-door-fill"></i> Home</a>
       <ul class="navbar-nav">
-        <?php if(!isset($_SESSION['id'])){ ?>
+        <?php if(!isset($_SESSION['id'])) { ?>
         <li class="nav-item">
           <a class="nav-link" aria-current="page" href="login.php"><i class="bi bi-pencil-square"></i> เข้าสู่ระบบ</a>
         </li>
         <?php }else{ ?>
             <li class="nav-item dropdown">
-          <a class="btn btn-outline-secondary btn-sm dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          <i class="bi bi-person-lines-fill"></i> <?php echo $_SESSION['username']; ?>
-          </a>
+            <a class="btn btn-outline-secondary btn-sm dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="bi bi-person-circle"></i> <?php echo $_SESSION['username']; ?>
+            </a>
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="logout.php"><i class="bi bi-power"></i> ออกจากระบบ</a></li>
           </ul>
-        </li>
-
+            </li>
         <?php } ?>
       </ul>
     </div>
 </nav>
-    <form>
-        หมวดหมู่ : 
-        <select>
-            <option value="all">--ทั้งหมด--</option>
-            <option value="general">เรื่องทั่วไป</option>
-            <option value="study">เรื่องเรียน</option>
-        </select>
+ 
+<div class="mt-3 d-flex justify-content-between">
+    <div>
+    <label>หมวดหมู่</label>
+        <span class="dropdown">
+            <button class="btn btn-light btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            --ทั้งหมด--
+        </button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">ทั้งหมด</a></li>
+            <li><a class="dropdown-item" href="#">เรื่องเรียน</a></li>
+            <li><a class="dropdown-item" href="#">เรื่องทั้งไป</a></li>
+        </ul>
+ 
+        </span>
+    </div>
+    <?php if(isset($_SESSION['id'])){ ?>
+        <div><a href="newpost.php" class="btn btn-success btn-sm"><i class="bi bi-plus-circle">
+        </i> สร้างกระทู้ใหม่</a></div>
+    <?php } ?>
+</div>
+ 
+    <table class="table table-striped mt-4 ">
         <?php
-        if (!isset($_SESSION['id'])){
-            echo"<a href=login.php style='float: right;'>เข้าสู่ระบบ</a>";
-        }else{
-            echo"<div style='float: right;'>
-            ผู้ใช้งานระบบ : $_SESSION[username]&nbsp;&nbsp;
-            <a href=logout.php>ออกจากระบบ</a>
-            </div><br>
-            <a href=newpost.php>สร้างกระทู้ใหม่</a>";
+        for($i=1;$i<=10;$i++){
+            echo "<tr><td class = 'd-flex justify-content-between'><a href = post.php?id=$i style = text-decoration:none>กระทู้ $i</a>";
+            if(isset($_SESSION['id']) && $_SESSION['role']=='a'){
+                echo "&nbsp;&nbsp;<a href=delete.php?id=$i
+                class = 'btn btn-danger btn-sm'><i class='bi bi-trash'></i></a>";
+            }
+            echo "</td></tr>";
         }
         ?>
-    </form>
-    <ul>
-    <?php 
-            for($i=1;$i<=10;$i++){
-                echo"<li><a href=post.php?id=$i>กระทู้ที่ $i</a>";
-                if(isset($_SESSION['id'])&& $_SESSION['role']=='a'){
-                    echo "&nbsp;&nbsp;<a href=delete.php?id=$i>ลบ</a>";
-              }
-              echo "</li>";
-            }
-        ?>
-    </ul>
-    </div>   
+    </table>
+    </div>
 </body>
 </html>
