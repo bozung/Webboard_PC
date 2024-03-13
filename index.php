598fe9c686ef13bc818a49af64b7b6b861b4ebd7
Phatphonnicha <151551494+bozung@users.noreply.github.com>
2024-03-13 19:16:32 +0700

    WORK 10 [part2] Java

--- index.php
+++ index.php
@@ -1,67 +1,83 @@
 <?php
-    session_start();
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-    
+
     <title>Webboard</title>
+    <script>
+    function myFunction() {
+        let r = confirm("ต้องการจะลบจริงหรือไม่ ??")
+        return r;
+    }
+    </script>
 </head>
 
 <body>
     <div class="container">
-    <h1 style="text-align: center;" class="mt-3">Webboard</h1>
-    <?php include "nav.php"; ?>
+        <h1 style="text-align: center;" class="mt-3">Webboard</h1>
+        <?php include "nav.php"; ?>
 
-    <div class="mt-3 d-flex justify-content-between">
-        <div>
-            <label>หมวดหมู่</label>
-            <span class="dropdown">
-                <button class="btn btn-light dropdown-toggle btn-sm"
-                 type="button" id ="Button2" data-bs-toggle="dropdown"
-                  aria-expanded="false">--ทั้งหมด--
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="Button2">
-                    <li><a href="#" class="dropdown-item">ทั้งหมด</a></li>
-                    <?php 
-                        $conn=new PDO("mysql:host=localhost;dbname=webboard;
-                        charset=utf8","root","");
-                        $sql="SELECT * FROM category";
-                        foreach($conn->query($sql) as $row){
+        <div class="mt-3 d-flex justify-content-between">
+            <div>
+                <label>หมวดหมู่</label>
+                <span class="dropdown">
+                    <button class="btn btn-light dropdown-toggle btn-sm" type="button" id="Button2"
+                        data-bs-toggle="dropdown" aria-expanded="false">--ทั้งหมด--
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="Button2">
+                        <li><a href="#" class="dropdown-item">ทั้งหมด</a></li>
+                        <?php
+                        $conn = new PDO("mysql:host=localhost;dbname=webboard;
+                        charset=utf8", "root", "");
+                        $sql = "SELECT * FROM category";
+                        foreach ($conn->query($sql) as $row) {
                             echo "<li><a class=dropdown-item href=#>$row[name]</a></li>";
                         }
-                        $conn=null;  
-                    ?>
-                </ul>
-            </span>
+                        $conn = null;
+                        ?>
+                    </ul>
+                </span>
+            </div>
+            <div><a href="newpost.php" class="btn btn-success btn-sm">
+                    <i class="bi bi-plus"></i>สร้างกระทู้ใหม่
+                </a></div>
         </div>
-        <div><a href="newpost.php" class="btn btn-success btn-sm">
-            <i class="bi bi-plus"></i>สร้างกระทู้ใหม่
-        </a></div>
-    </div>
 
 
-    <table class="table table-striped mt-4">
-        <?php
-        $conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8", "root", "");
-        $sql="SELECT t3.name,t1.title,t1.id,t2.login,t1.post_date FROM post as t1 
+        <table class="table table-striped mt-4">
+            <?php
+            $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8", "root", "");
+            $sql = "SELECT t3.name,t1.title,t1.id,t2.login,t1.post_date FROM post as t1 
         INNER JOIN user as t2 ON (t1.user_id=t2.id)
         INNER JOIN category as t3 ON (t1.cat_id=t3.id) ORDER BY t1.post_date DESC";
-        $result=$conn->query($sql);
-        while($row = $result->fetch()){
-            echo "<tr><td>[ $row[0] ] <a href=post.php?id=$row[2] 
-            style=text-decoration:none>$row[1]</a><br>$row[3] - $row[4]</td></tr>";
-        }
-        $conn=null;   
-        ?>
-</table>
-    
+            $result = $conn->query($sql);
+            while ($row = $result->fetch()) {
+                echo "<tr><td class='d-flex justify-content-between'>
+                <div>[ $row[0] ] <a href=post.php?id=$row[2] 
+                style=text-decoration:none>$row[1]</a><br>$row[3] - $row[4]</div>";
+                if(isset($_SESSION['id']) && $_SESSION['role']=='a'){
+                    echo "<div class='me-2 align-self-center'><a href=delete.php?id=$row[2]
+                    class='btn btn-danger btn-sm' onclick='return myFunction()'><i class='bi bi-trash'</i></a></div>";
+                }
+                echo "</td></tr>";
+            }
+            $conn = null;
+            ?>
+        </table>
+
     </div>
-</body>    
+</body>
+
 </html>
--- register.php
+++ register.php
@@ -19,26 +19,36 @@ if (isset($_SESSION["id"])) {
 </head>
 
 <body>
+    <script>
+        function OnBluePwd() {
+            let pwd = document.getElementById("pwd");
+            let pwd2 = document.getElementById("pwd2");
+            if (pwd.value !== pwd2.value) {
+                alert("รหัสผ่านทั้งสองช่องไม่ตรงกัน");
+                pwd2.value = "";
+            }
+        }
+    </script>
     <div class="container">
-        <h1 style="text-align: center;" class="mt-3">Webboard </h1>
+        <h1 style="text-align: center;" class="mt-3">Webboard</h1>
         <hr>
         <?php
         include "nav.php"
-        ?>
+            ?>
         <div class="row mt-4">
             <div class="col-lg-4 col-md-2 col-sm-1"></div>
             <div class="col-lg-6 col-md-8 col-sm-10">
-                <?php 
-                    if(isset($_SESSION['add_login'])){
-                        if($_SESSION['add_login']=="error"){
-                            echo "<div class ='alert alert-danger'>
+                <?php
+                if (isset($_SESSION['add_login'])) {
+                    if ($_SESSION['add_login'] == "error") {
+                        echo "<div class ='alert alert-danger'>
                             ชื่อบัญชีซ้ำหรือฐานข้อมูลมีปัญหา</div>";
-                        }else{
-                            echo "<div class ='alert alert-success'>
+                    } else {
+                        echo "<div class ='alert alert-success'>
                             เพิ่มบัญชีเรียบร้อยแล้ว</div>";
-                        }
-                        unset($_SESSION['add_login']);
                     }
+                    unset($_SESSION['add_login']);
+                }
                 ?>
                 <div class="card border-primary">
                     <div class="card-header bg-primary text-white">
@@ -55,7 +65,15 @@ if (isset($_SESSION["id"])) {
                             <div class="row mt-3">
                                 <label for="pwd" class="col-lg-3 col-form-label">รหัสผ่าน</label>
                                 <div class="col-lg-9">
-                                    <input id="pwd" type="password" name="pwd" class="form-control" required>
+                                    <input type="password" name="pwd" id="pwd" class="form-control" required>
+                                </div>
+                            </div>
+
+                            <div class="row mt-3">
+                                <label for="pwd" class="col-lg-3 col-form-label">ใส่รหัสผ่านซ้ำ</label>
+                                <div class="col-lg-9">
+                                    <input type="password" name="pwd2" id="pwd2" onblur="OnBluePwd()"
+                                        class="form-control" required>
                                 </div>
                             </div>
                             <div class="row mt-3">
@@ -68,35 +86,42 @@ if (isset($_SESSION["id"])) {
                                 <label class="col-lg-3 form-label">เพศ</label>
                                 <div class="col-lg-9">
                                     <div class="form-check">
-                                        <input id="m" type="radio" name="gender" value="m" class="form-form-check-input" required>
+                                        <input id="m" type="radio" name="gender" value="m" class="form-form-check-input"
+                                            required>
                                         <label for="m" class="form-check-label">ชาย</label>
                                     </div>
                                     <div class="form-check">
-                                        <input id="f" type="radio" name="gender" value="m" class="form-form-check-input" required>
+                                        <input id="f" type="radio" name="gender" value="m" class="form-form-check-input"
+                                            required>
                                         <label for="f" class="form-check-label">หญิง</label>
                                     </div>
                                     <div class="form-check">
-                                        <input id="o" type="radio" name="gender" value="m" class="form-form-check-input" required>
+                                        <input id="o" type="radio" name="gender" value="m" class="form-form-check-input"
+                                            required>
                                         <label for="o" class="form-check-label">อื่นๆ</label>
                                     </div>
                                 </div>
                             </div>
                             <div class="row mt-3">
                                 <label for="email" class="col-lg-3 col-form-label">อีเมล</label>
-                                <div class="col-lg-9"><input id="email" type="email" name="email" class="form-control" required></div>
+                                <div class="col-lg-9"><input id="email" type="email" name="email" class="form-control"
+                                        required></div>
                             </div>
                             <div class="row mt-3">
                                 <div class="col-lg-12 d-flex justify-content-center">
-                                    <button type="submit" class="btn btn-primary btn-sm me-2"> <i class="bi bi-save"></i> สมัครสมาชิก</button>
-                                    <button type="reset" class="btn btn-danger btn-sm "> <i class="bi bi-x-square"></i> ยกเลิก</button>
+                                    <button type="submit" class="btn btn-primary btn-sm me-2"> <i
+                                            class="bi bi-save"></i> สมัครสมาชิก</button>
+                                    <button type="reset" class="btn btn-danger btn-sm "> <i class="bi bi-x-square"></i>
+                                        ยกเลิก</button>
                                 </div>
                             </div>
                         </form>
                     </div>
                 </div>
             </div>
-                <div class="col-lg-3 col-md-2 col-sm-1"></div>
+            <div class="col-lg-3 col-md-2 col-sm-1"></div>
         </div>
     </div>
 </body>
+
 </html>
